7f32cc13fc53755aa0d95aba5f5ed50194ad9dcb
Abonty18 <labibafarah2998@gmail.com>
2022-10-24 19:44:37 +0600

    duck project

---
+++ Observable.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Observable implements QuackObservable {
+    ArrayList observers = new ArrayList();
+    QuackObservable duck;
+    public Observable(QuackObservable duck) {
+        this.duck = duck;
+    }
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers() {
+        Iterator iterator = observers.iterator();
+        while (((Iterator<?>) iterator).hasNext()) {
+            Observer observer = (Observer)iterator.next();
+            observer.update(duck);
+        }
+    }
+}
